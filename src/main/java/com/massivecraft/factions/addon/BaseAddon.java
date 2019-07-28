75b8a4bdc2a28e6fe55d936bb33519d2fcf0e662
prosavage <naman0311@gmail.com>
2019-07-28 01:34:08 -0500

    Dont initialize lists

--- src/main/java/com/massivecraft/factions/addon/BaseAddon.java
+++ src/main/java/com/massivecraft/factions/addon/BaseAddon.java
@@ -2,13 +2,12 @@ package com.massivecraft.factions.addon;
 
 import com.massivecraft.factions.cmd.FCommand;
 
-import java.util.Collections;
 import java.util.List;
 
 public abstract class BaseAddon {
 
-    protected static List<Object> listeners = Collections.emptyList();
-    protected static List<FCommand> fCommands = Collections.emptyList();
+    protected static List<Object> listeners;
+    protected static List<FCommand> fCommands;
     protected String addonIdentifier;
 
     public abstract List<Object> getListeners();
